2567f944873993df8860d294a5540946907322a0
imanirak <karinadgarces@gmail.com>
2022-04-17 20:14:12 -0400

    user login/logout/signup functional

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,6 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.Home.as_view(), name='home'),
     path('about', views.About.as_view(), name='about'),
+    #signup
+    path('accounts/signup/', views.signup_view, name='signup')
 
 
     
