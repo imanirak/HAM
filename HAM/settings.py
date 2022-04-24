b35940fa37f04db6db2be1c313f736913e92a54e
imanirak <karinadgarces@gmail.com>
2022-04-24 16:10:08 -0400

    nav bar, login/logout views

--- HAM/settings.py
+++ HAM/settings.py
@@ -119,6 +119,6 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_URL = '/accounts/login/'
-LOGIN_REDIRECT_URL = '/'
+LOGIN_REDIRECT_URL = '/devices/'
 LOGOUT_REDIRECT_URL = "/"
 django_heroku.settings(locals())
