c6a1dcd2ccb02cadf9c39a589baa3e92c143519f
imanirak <karinadgarces@gmail.com>
2022-04-25 17:46:00 -0400

    auto scroll functional

--- HAM/settings.py
+++ HAM/settings.py
@@ -17,8 +17,8 @@ STATIC_ROOT = BASE_DIR / 'static'
 SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
-DEBUG = False
+DEBUG = True
+#DEBUG = False
 
 ALLOWED_HOSTS = []
 
